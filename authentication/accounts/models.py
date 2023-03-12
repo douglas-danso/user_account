462fdee0592b99dcd3e59bd9691e298a8d56d4a0
douglas-danso <dogsteins1@gmail.com>
2023-03-12 05:02:04 +0000

    model created

--- authentication/accounts/models.py
+++ authentication/accounts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class CuatomUserManager(BaseUserManager):
+    def create_user(self, username, email, password = None, **extra_fields):
+        if not email:
+            raise ValueError('Email Address is Required')
+        
+        if not password:
+            raise ValueError('Password is Required')
+        try:
+                user = self.model(
+                    email = self.normalize_email(email),
+                    username =username
+                    **extra_fields 
+                )
+                user.set_password(password)
+                user.save()
+                return user
+        except:
+             raise ValueError('An Error Occured Please Try Again') 
+        
+
+    def create_superuser(self, email, username,password=None, **extra_fields):
+        try:
+            user = self.create_user(
+                    email,
+                    username=username,
+                    password=password,
+                    is_admin=True,
+                    is_superuser=True,
+                    is_staff=True,
+                    **extra_fields,
+            )
+            
+            return user
+        except:
+               raise ValueError('An Error Occured Please Try Again')
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    # USERNAME_FIELD = username
+    REQUIRED_FIELDS = ['email']
